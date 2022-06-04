807268dab0095251a32d94711f78df547f5278c2
Nick Timkovich <nick@telnyx.com>
2022-06-03 21:41:41 -0500

    Block simultaneous pyvenv bootstrapping

--- scripts/make-delegator.sh
+++ scripts/make-delegator.sh
@@ -8,7 +8,7 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 REPO_ROOT="$(dirname "$SCRIPT_PATH")"
 VENV_DIR="${REPO_ROOT}/.venv"
 
-bash "${SCRIPT_PATH}/pyvenv-bootstrap.sh"
+flock --exclusive --timeout 15 "${SCRIPT_PATH}/pyvenv-bootstrap.sh" -c "bash \"${SCRIPT_PATH}/pyvenv-bootstrap.sh\""
 
 # "$VENV_DIR/bin/python3" -m invoke $@
 "$VENV_DIR/bin/python3" -m doit $@
